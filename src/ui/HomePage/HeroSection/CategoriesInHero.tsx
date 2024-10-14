2133a7d2b5e6f225a5bbbc8c7473439c834aee8f
Yash Jha <amitjha329@gmail.com>
2024-10-14 17:27:47 +0530

    tried to resolve cartegory image issue

--- src/ui/HomePage/HeroSection/CategoriesInHero.tsx
+++ src/ui/HomePage/HeroSection/CategoriesInHero.tsx
@@ -9,9 +9,9 @@ const CategoriesInHero = async () => {
       {
         categories.map((category) => {
           return (
-            <div className='space-y-2 min-h-32' key={category._id}>
+            <div className='space-y-2 ~min-h-32/40' key={category._id}>
               <div className='~w-16/24 ~h-20/28 overflow-hidden rounded-xl border-white border-2 relative mx-auto'>
-                <Image src={category.imgSrc} alt={category.name} fill className='object-cover' priority />
+                <Image src={category.imgSrc} alt={category.name} height={112} width={96} className='object-cover' priority />
               </div>
               <div className='text-xs max-w-24 text-center'>{category.name}</div>
             </div>
