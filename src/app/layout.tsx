b680481eb4e2b5102573542494dc9e0893735891
Yash Jha <amitjha329@gmail.com>
2024-10-18 12:24:25 +0530

    Added No INdex TO The Site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import '@/styles/globals.scss'
 import { Poppins } from 'next/font/google'
 import clsx from 'clsx'
 import NextTopLoader from 'nextjs-toploader'
-import { Viewport } from 'next'
+import { Metadata, Viewport } from 'next'
 
 const poppins = Poppins({
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -14,9 +14,16 @@ export const viewport: Viewport = {
     initialScale: 1.0
 }
 
+export const metadata: Metadata = {
+
+}
+
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
         <html>
+            <head>
+                <meta name="robots" content="noindex,nofollow" />
+            </head>
             <body className={clsx(poppins.className)}>
                 <NextTopLoader
                     color='#6c4624'
